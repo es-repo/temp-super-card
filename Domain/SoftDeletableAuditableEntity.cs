7e912afebb1639ae62d8d0f3735b2108651eb0bd
Evgheni Sirota <eugenesirota@gmail.com>
2018-10-15 17:44:26 +0300

    Update Domain

---
+++ Domain/SoftDeletableAuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+	public abstract class SoftDeletableAuditableEntity : AuditableEntity, ISoftDeletable
+	{
+		public bool IsDeleted => DeletedOn.HasValue;
+
+		public DateTimeOffset? DeletedOn { get; set; }
+
+		public string DeletedBy { get; set; }
+	}
+}
